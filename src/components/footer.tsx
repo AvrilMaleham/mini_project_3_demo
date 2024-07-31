e2e17426f1ba1f3fbb7fbd111fa95f261595ef55
Avril Maleham <avrilmaleham@outlook.com>
2024-08-01 10:50:39 +1200

    feat: create footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="py-3 container text-center">
+        <p>&copy; 1999 Slice of Heaven. All rights reserved.</p>
+        <p>
+          Follow us on: <a href="https://www.facebook.com/DominosNZ/">Facebook</a> | <a href="https://x.com/dominos">Twitter</a> | <a href="https://www.instagram.com/dominos_nz/">Instagram</a>
+        </p>
+    </footer>
+  );
+}
+
+export default Footer;
