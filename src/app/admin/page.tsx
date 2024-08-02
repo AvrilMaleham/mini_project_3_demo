a98b31f715ada5852a4f7aab950e06c66db6b6da
Avril Maleham <avrilmaleham@outlook.com>
2024-08-03 10:22:18 +1200

    feat:add authorization

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,17 @@
+import { options } from "../api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
+import { redirect } from "next/navigation"
 import { Order } from '@/types';
 import Accordion from '@/components/accordion';
 import PageHeader from "@/components/page-header";
 
 const Admin: React.FC = async () => {
+  const session = await getServerSession(options)
+
+  if (!session) {
+      redirect('/api/auth/signin?callbackUrl=/admin')
+  }
+
   const res = await fetch("http://localhost:3000/api/orders?status=new");
   const orders: Order[] = await res.json();
 
